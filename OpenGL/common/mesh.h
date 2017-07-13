8f3ec8c9d3eae963a0b3289de9eec1b558b7d1f0
sofiawu <sofiawu@tencent.com>
2017-07-13 10:06:42 +0800

    load model

---
+++ OpenGL/common/mesh.h
@@ -0,0 +1,13 @@
+//
+//  mesh.h
+//  OpenGL
+//
+//  Created by sofiawu on 2017/7/12.
+//  Copyright © 2017年 sofiawu. All rights reserved.
+//
+
+#ifndef mesh_h
+#define mesh_h
+
+
+#endif /* mesh_h */
---
+++ OpenGL/common/model.h
@@ -0,0 +1,13 @@
+//
+//  model.h
+//  OpenGL
+//
+//  Created by sofiawu on 2017/7/13.
+//  Copyright © 2017年 sofiawu. All rights reserved.
+//
+
+#ifndef model_h
+#define model_h
+
+
+#endif /* model_h */
